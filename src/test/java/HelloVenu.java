2372a46da2c51610ecd7a07c1d74a4b585def419
venu2804 <venu2804@gmail.com>
2018-10-05 20:17:45 +0100

    writen a new print command

--- src/test/java/HelloVenu.java
+++ src/test/java/HelloVenu.java
@@ -2,5 +2,8 @@ public class HelloVenu {
 
     public static void main(String[] args) {
         System.out.println("Hello Venu");
+
+        System.out.println("my second print command");
+                
     }
 }
